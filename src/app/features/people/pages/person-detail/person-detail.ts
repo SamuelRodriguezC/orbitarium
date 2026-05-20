5b95bc1aad4edd474156f3f1570a3ac1c6e13a1f
SamuelRodríguezC <samuelrc250@gmail.com>
2026-05-20 17:48:23 -0500

    feat(person-detail): crear componente para detalle de personaje

---
+++ src/app/features/people/pages/person-detail/person-detail.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-person-detail',
+  imports: [],
+  templateUrl: './person-detail.html',
+  styleUrl: './person-detail.css',
+})
+export class PersonDetail {}
